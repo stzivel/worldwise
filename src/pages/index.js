e501ce71a1cc2a3f158f06409def2ce9c1834014
spyros <stzivel@gmail.com>
2023-09-13 20:23:39 +0300

    Added dynamic imports to pages for optimization

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as AppLayout } from "./AppLayout";
-export { default as Login } from "./Login";
-export { default as Homepage } from "./Homepage";
-export { default as PageNotFound } from "./PageNotFound";
-export { default as Pricing } from "./Pricing";
-export { default as Product } from "./Product";
-export { default as ProtectedRoute } from "./ProtectedRoute";
